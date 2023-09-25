f5e65c4e7a9e449b812c451c9bc1d28c05bfc5c4
Taylor Laird <taylorolaird@knights.ucf.edu>
2023-09-25 19:41:04 -0400

    added pathing for images

--- glip.py
+++ glip.py
@@ -1,5 +1,6 @@
 import json
-
+from PIL import Image
+import glob
 
 def load_json(file):
     f = open(file)
@@ -8,26 +9,35 @@ def load_json(file):
     return data
 
 
+def laod_image(file):
+    image_list = []
+    for filename in glob.glob(f'{file}*.jpg'):
+        im = Image.open(filename)
+        image_list.append(im)
+
+
 def get_data():
     captions = load_json('../../../groups/course.cap6411/Dataset/coco/annotations/captions_val2017.json')
     instances = load_json('../../../groups/course.cap6411/Dataset/coco/annotations/instances_val2017.json')
     person_keypoints = load_json('../../../groups/course.cap6411/Dataset/coco/annotations/person_keypoints_val2017.json')
+    images = laod_image('../../../groups/course.cap6411/Dataset/coco/val2017/*.jpg')
 
     return captions, instances, person_keypoints
 
 
 captions, instances, person_keypoints = get_data()
 
-for i in captions['annotations']:
-    print(i['caption'])
+# for i in captions['annotations']:
+#     print(i['caption'])
+#
+# print('=' * 100)
+#
+# for i in instances['annotations']:
+#     print(i['category_id'])
+#
+# print('=' * 100)
+#
+# for i in person_keypoints['annotations']:
+#     print(i['keypoints'])
 
-print('=' * 100)
-
-for i in instances['annotations']:
-    print(i['category_id'])
-
-print('=' * 100)
-
-for i in person_keypoints['annotations']:
-    print(i['keypoints'])
 
